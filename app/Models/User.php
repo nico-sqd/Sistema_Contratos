9fa1be72b7689d9eef38f68621cf755f07f4f3c3
FelipeGuzmanL <felipeeduardo.guzmanlobos@gmail.com>
2022-05-08 16:52:48 -0400

    Creacion de seeder donde crea tabla establecimientos completa

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,4 +43,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function getEstablecimiento()
+    {
+        return $this->belongsTo(Establecimiento::class,'establecimiento');
+    }
 }
