ebef8c2d7d4e2b6e8a0cb97d6d6af755da9c48d1
FelipeGuzmanL <felipeeduardo.guzmanlobos@gmail.com>
2022-05-08 17:07:07 -0400

    Creacion de seeder que crea tabla establecimiento completa

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use App\Models\User;
 
 class UserSeeder extends Seeder
@@ -14,10 +15,51 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('establecimiento')->insert([
+            'establecimiento'=>'Servicio Salud Osorno',
+            'abreviacion'=>'DSSO',
+            'codigo_deis'=>'123010'
+        ]);
+        DB::table('establecimiento')->insert([
+            'establecimiento'=>'Hospital Base San José Osorno',
+            'abreviacion'=>'HBSJO',
+            'codigo_deis'=>'123100'
+        ]);
+        DB::table('establecimiento')->insert([
+            'establecimiento'=>'Hospital Futa Sruka Lawenche Kunko Mapu Mo',
+            'abreviacion'=>'HFSLKMM',
+            'codigo_deis'=>'123104'
+        ]);
+        DB::table('establecimiento')->insert([
+            'establecimiento'=>'Hospital Purranque',
+            'abreviacion'=>'HPU',
+            'codigo_deis'=>'123101'
+        ]);
+        DB::table('establecimiento')->insert([
+            'establecimiento'=>'Hospital Pu Mülen Quilacahuín',
+            'abreviacion'=>'HPMQ',
+            'codigo_deis'=>'123105'
+        ]);
+        DB::table('establecimiento')->insert([
+            'establecimiento'=>'Hospital Puerto Octay',
+            'abreviacion'=>'HPO',
+            'codigo_deis'=>'123103'
+        ]);
+        DB::table('establecimiento')->insert([
+            'establecimiento'=>'Hospital Río Negro',
+            'abreviacion'=>'HRN',
+            'codigo_deis'=>'123102'
+        ]);
+        DB::table('establecimiento')->insert([
+            'establecimiento'=>'Ninguno',
+            'abreviacion'=>'NN',
+            'codigo_deis'=>'1'
+        ]);
         $user = User::create([
             'name'=>'Admin',
             'email'=>'admin@admin.com',
-            'rut'=>'11111111-1',
+            'rut'=>'12345678-9',
+            'establecimiento'=>'1',
             'password'=>bcrypt('admin12'),
         ]);
 
